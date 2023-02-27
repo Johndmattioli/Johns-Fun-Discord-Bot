c6698025883ca21631120a346c5235e39f56feb2
John Mattioli <johndmattioli@gmail.com>
2023-02-27 02:00:04 -0600

    feat: Added SQL Database Connection and Some Cleanup

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const Eris = require("eris")
 const api = require("axios")
+var mysql = require('mysql');
+
 require('dotenv').config()
 //var cron = require('node-cron')
 const Constants = Eris.Constants
+var wasLastMsgAddEvent = false;
+var referenceId = "";
+
+var con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: process.env.DB_NAME
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.BOT_KEY, {
@@ -12,7 +28,7 @@ const bot = new Eris(process.env.BOT_KEY, {
 });
 
 bot.on("ready", () => { // When the bot is ready
-    bot.createMessage("1076576128837365850","John's Fun Bot is up and running!");
+    bot.createMessage("1076576128837365850", "John's Fun Bot is up and running!");
     console.log("Ready!"); // Log "Ready!"
 });
 
@@ -20,28 +36,52 @@ bot.on("error", (err) => {
     console.error(err); // or your preferred logger
 });
 
-/**
- * cron.schedule('* * * * *', () => {
-    console.log("Scheduled Message Begins");
-    bot.createMessage("1076576128837365850", "This is a scheduled message executing every minute. Current Time is " + new Date());
-});
- */
-
 bot.on("messageCreate", (msg) => { // When a message is created
     var authorTheBot = isTheAuthorTheBot(msg.author.username);
     var discChannelId = msg.channel.id;
     var discName = msg.author.username;
     if (!authorTheBot) { //This is necessary because the bot was responding to its own messages causing an infinite loop
-        switch (msg.content) {
-            case "get the news titles":  bot.createMessage(discChannelId, retrieveNewsTitlesNexus().toString());
-                break;
-            case "hey bot suck my ass": bot.createMessage(discChannelId, "No " + discName + " you suck my ass");
-                break;
-            case "how many times does egan dab each day?": bot.createMessage(discChannelId, "UNEXPECTED DATA TYPE FAULT: Maximum value for a variable of type int = 2147483647");
-                break;
-            case "!select": bot.createMessage(discChannelId, createDiscordButtonHelper());
-                break;
-            default: bot.createMessage(discChannelId, "Hi " + discName + " I don't have any logic mapped for that command please try again. Also u suck lol");
+        if (wasLastMsgAddEvent) {
+            var todaysDate = new Date().toISOString().split('T')[0];
+            var sql = "INSERT INTO events (event_content, event_date) VALUES ('" + msg.content + "','" + todaysDate + "')";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log("1 record inserted");
+                bot.createMessage(discChannelId, "Your record has been recorded. Please type 'get events' in order to retrieve your stored events.");
+                wasLastMsgAddEvent = false;
+            });
+        } else {
+            switch (msg.content.toUpperCase()) {
+                case "HI":
+                case "HELLO":
+                case "YO":
+                case "HOWDY": bot.createMessage(discChannelId, "Hey there: " + discName + "!");
+                    break;
+                case "GET THE NEWS TITLES": bot.createMessage(discChannelId, retrieveNewsTitlesNexus().toString());
+                    break;
+                case "HEY BOT SUCK MY ASS": bot.createMessage(discChannelId, "No " + discName + " you suck my ass");
+                    break;
+                case "HOW MANY TIMES DOES EGAN DAB EACH DAY?": bot.createMessage(discChannelId, "UNEXPECTED DATA TYPE FAULT: Maximum value for a variable of type int = 2147483647");
+                    break;
+                case "!SELECT": bot.createMessage(discChannelId, createDiscordButtonHelper());
+                    break;
+                case "ADD EVENT": 
+                    bot.createMessage(discChannelId, "What content would you like to store?");
+                    wasLastMsgAddEvent = true;
+                    break;
+                case "GET EVENTS":
+                    con.query("SELECT * FROM events", function (err, result, fields) {
+                        if (err) throw err;
+                        console.log(result);
+                        var presentedData = [];
+                        result.forEach(function(item) {
+                            presentedData.push("" + item.event_content + " ");
+                        })
+                        bot.createMessage(discChannelId, {embed: buildEmbeddedList(presentedData)});
+                    });
+                    break;
+                default: bot.createMessage(discChannelId, "Hi " + discName + " I don't have any logic mapped for that command please try again. Also u suck lol");
+            }
         }
     }
 });
@@ -63,6 +103,31 @@ const createDiscordButtonHelper = () => {
                 ]
             }
         ]
+    };
+}
+
+const buildEmbeddedList = (presentedData) => {
+    return {
+        title: "List Of Recorded Events",
+        description: presentedData.toString(),
+        url: "https://blob-project.com",
+        timestamp: new Date(),
+        color: 0x7289DA,
+        footer: {
+            text:"That was the event list that is stored in the database."
+        },
+        image: {
+            url:"https://i.ibb.co/BrkS6BX/unboost.png"
+        },
+        thumbnail: {
+            url: "https://i.ibb.co/BrkS6BX/unboost.png"
+        },
+        fields: [
+            {name: "This is a field", value: "This is a field (value", inline:true}
+        ],
+        author: {
+            name: "This is an author", url: "https://blob-project.com"
+        }
     };
 }
 
